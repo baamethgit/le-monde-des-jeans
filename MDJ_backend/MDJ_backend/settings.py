5485be29cd7577460c70691ece7974e5ea77fcf8
BA_Ameth <baameth@ept.sn>
2024-09-13 12:58:32 +0000

    creation .env

--- MDJ_backend/MDJ_backend/settings.py
+++ MDJ_backend/MDJ_backend/settings.py
@@ -73,12 +73,23 @@ WSGI_APPLICATION = "MDJ_backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "le_monde_des_jeans",
+        "USER":"baameth",
+        "PASSWORD":"postgreSQL",
+        "HOST":"localhost",
     }
 }
+
 
 
 # Password validation
