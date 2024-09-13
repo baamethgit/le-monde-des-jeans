216e7e2cb1a68a025534ac684919d21e1a7db526
BA_Ameth <baameth@ept.sn>
2024-09-13 20:30:27 +0000

    update gitignore,.env creation applis

--- MDJ_backend/MDJ_backend/settings.py
+++ MDJ_backend/MDJ_backend/settings.py
@@ -17,7 +17,7 @@ env = environ.Env()
 environ.Env.read_env()
 
 # Your secret key
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = "django-insecure-@4auoqe$vcb&kapw2%ol_v7i*&-qht#rv&j=!1-9%(jv%2de"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "shop"
 ]
 
 MIDDLEWARE = [
@@ -79,23 +80,23 @@ WSGI_APPLICATION = "MDJ_backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env("DB_NAME"),
-        'USER': env("DB_USER"),
-        'PASSWORD': env("DB_PASSWORD"),
-        'HOST': env("DB_HOST"),
-        'PORT': env("DB_PORT"),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': env("DB_NAME"),
+#         'USER': env("DB_USER"),
+#         'PASSWORD': env("DB_PASSWORD"),
+#         'HOST': env("DB_HOST"),
+#         'PORT': env("DB_PORT"),
+#     }
+# }
 
 
 
