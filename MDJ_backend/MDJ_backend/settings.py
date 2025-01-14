7061c6887b8e04d92efcc8855f0a36832a1fb7a4
amethba <baameth@ept.sn>
2025-01-14 13:21:46 +0000

    connect to remote postgresql db , fixing migrations issues

--- MDJ_backend/MDJ_backend/settings.py
+++ MDJ_backend/MDJ_backend/settings.py
@@ -95,6 +95,7 @@ WSGI_APPLICATION = "MDJ_backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+"""
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
@@ -104,7 +105,7 @@ DATABASES = {
 """
 DATABASES = {
      'default': {
-         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+         'ENGINE': 'django.db.backends.postgresql',
          'NAME': env("DB_NAME"),
          'USER': env("DB_USER"),
          'PASSWORD': env("DB_PASSWORD"),
@@ -112,8 +113,6 @@ DATABASES = {
          'PORT': env("DB_PORT"),
      }
 }
-"""
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
