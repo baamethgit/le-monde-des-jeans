32b1e63a5dad74a72166289314da3c5d35876f1f
Cr4zy5h4rk <f4ll0utz3r0@gmail.com>
2025-01-29 00:19:58 +0000

    correction page avis

--- MDJ_frontEnd/src/app/components/avis/avis.component.ts
+++ MDJ_frontEnd/src/app/components/avis/avis.component.ts
@@ -3,25 +3,38 @@ import { UserService } from '../../services/users/user.service';
 import { Avis } from '../../models/Avis';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { NgbPagination } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-avis',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, NgbPagination],
   templateUrl: './avis.component.html',
   styleUrl: './avis.component.scss'
 })
 export class AvisComponent {
 Avis:Avis[]=[];
+pageSize: number = 20;
+totalItems: number = 0;
+page: number = 1;
 
   constructor(private readonly userService:UserService){}
 
 ngOnInit():void{
-  this.userService.getAllAvis().subscribe({
-    next:(data:Avis[])=>{
-      this.Avis=data
+    this.loadAvis();
+}
+
+loadAvis():void{
+  this.userService.getAllAvis(this.page,this.pageSize).subscribe({
+    next:(data)=>{
+      this.Avis=data.results;
+      this.totalItems=data.count
     }
 
   })
+}
+onPageChange(page: number) {
+  this.page = page;
+  this.loadAvis();
 }
 }
