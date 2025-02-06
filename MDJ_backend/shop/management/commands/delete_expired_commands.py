69688e850d5f60a0cfbbb4c93c6ca89623642163
amethba <baameth@ept.sn>
2025-02-06 18:56:58 +0000

    fix crontab actions

--- MDJ_backend/shop/management/commands/delete_expired_commands.py
+++ MDJ_backend/shop/management/commands/delete_expired_commands.py
@@ -1,19 +1,42 @@
 from django.core.management.base import BaseCommand
 from django.utils import timezone
+from django.db import transaction
 from shop.models import Commande
 from loguru import logger
 
+
 class Command(BaseCommand):
-    help = 'Supprime les commandes en attente expirées'
+    help = 'Supprime les commandes en attente expirées et restaure les stocks'
 
     def handle(self, *args, **kwargs):
+        try:
+            with transaction.atomic():
+                commandes_expirees = Commande.objects.filter(
+                    statut='EN_ATTENTE',
+                    date_expiration__lt=timezone.now()
+                )
 
-        commandes_expirees = Commande.objects.filter(
-            statut='EN_ATTENTE',
-            date_expiration__lt=timezone.now()
-        )
+                count = commandes_expirees.count()
 
-        count = commandes_expirees.count()
-        commandes_expirees.delete()
-        logger.info(f'[CRONJOB] : {count} commandes en attente expirées supprimées.')
-        self.stdout.write(self.style.SUCCESS(f'{count} commandes en attente expirées supprimées.'))
+                for commande in commandes_expirees:
+                    for produit in commande.produits.all():
+                        produit.QuantiteStock += 1
+                        produit.save()
+
+                    # Supprimer la commande
+                    commande.delete()
+
+                logger.info(f'[CRONJOB] : {count} commandes en attente expirées supprimées et stocks restaurés.')
+                self.stdout.write(
+                    self.style.SUCCESS(
+                        f'{count} commandes en attente expirées supprimées et stocks restaurés.'
+                    )
+                )
+
+        except Exception as e:
+            logger.error(f'[CRONJOB] Erreur lors du traitement des commandes expirées: {str(e)}')
+            self.stdout.write(
+                self.style.ERROR(
+                    f'Erreur lors du traitement des commandes expirées: {str(e)}'
+                )
+            )
