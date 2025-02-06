796f00cfca4eb4b958da5dfeb661d53f9a2480f8
Cr4zy5h4rk <f4ll0utz3r0@gmail.com>
2025-02-06 15:51:20 +0000

    roforfdd

--- MDJ_frontEnd/src/app/components/admin/payment/payment.component.ts
+++ MDJ_frontEnd/src/app/components/admin/payment/payment.component.ts
@@ -52,7 +52,6 @@ export class PaymentComponent implements OnInit{
       {
         next:(data)=>{
           this.payments = data;
-          console.log("paiements : ", data)
         }
       }
     )
